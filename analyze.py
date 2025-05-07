838b3617b7d77e433fcbdb68d8bbd1a41ed9346e
ManzHoffman <39713754+ManzHoffman@users.noreply.github.com>
2025-05-07 16:42:32 +0200

    first steps
    Adding first markup

---
+++ analyze.py
@@ -0,0 +1,28 @@
+import re
+
+
+# This script analyze
+path_of_clean_txt="martin_clean.txt"
+output_path="result/martin_clean.xml"
+
+
+
+#we open the clean text
+text = open(path_of_clean_txt, encoding="utf-8").read()
+
+text = re.sub(r'^(SCÈNE\s[IVXLCDM]{1,6})', r'<head>\1</head>\n', text, flags=re.MULTILINE)
+
+text = re.sub(r'\(_(.*?)_\)', r'<stage>\1</stage>\n', text, flags=re.MULTILINE)
+
+
+# insert 
+text = re.sub(r'^([A-ZÉÈÀÙÂÊÎÔÛÄËÏÖÜÇ]+)\.\n', r'<sp><speaker>\1</speaker><p>\n', text, flags=re.MULTILINE)
+
+text = re.sub(r'\n(?=[A-ZÉÈÀÙÂÊÎÔÛÄËÏÖÜÇ]+\.\n)', r'</p></sp>\n', text)
+
+# Add XML header and root tag (optional, for proper XML structure)
+text = '<?xml version="1.0" encoding="UTF-8"?>\n<body>\n' + text + '\n</body>'
+
+# Export to XML file
+with open(output_path, "w", encoding="utf-8") as output_file:
+    output_file.write(text)
---
+++ minimal-html.xsl
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+
+  <xsl:output method="html"/>
+
+  <xsl:template match="/">
+    <html>
+      <head>
+        <title>No Title</title>
+      </head>
+      <body>
+        <xsl:apply-templates/>
+      </body>
+    </html>
+  </xsl:template>
+</xsl:stylesheet>
